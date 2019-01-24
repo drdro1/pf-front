e39e6c4d887fa205784c162f606f4fe9b8153932
tarik harzafi <harzafi7@gmail.com>
2019-01-24 15:21:33 +0100

    added several starter files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav/navbar.component';
+
+import { appRoutes } from './routes';
+import { SpecificsComponent } from './ft/specifics.component';
+import { MetricsComponent } from './ft/metrics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SpecificsComponent,
+    MetricsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
